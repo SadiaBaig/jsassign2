7cf817e9436e598d5d237e2e0ae95a32c6e7770e
SadiaBaig <57152119+SadiaBaig@users.noreply.github.com>
2020-06-21 14:30:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//chap 21 to 38 task1
+// // var firstName = 'Erik';
+// // var lastName = 'Krieg';
+// // var fullName = firstName + ' ' + lastName;   
+// var fullName = alert("hi "+ fullName); 
+
+//chap 21 to 38 task2
+// var str = prompt("Enter mobile model");  
+// var numChars = str.length; 
+// for (var i = 0; i < numChars; i++) { 
+// document.write(numChars)    
+// }
+//chap 21 to 38 task4
+// for (var i = 0; i < text.length; i++) { 
+//      if (text.charAt(i) === "HELLO WORLD") { 
+//             alert("LAST INDEX OF  L!"); 
+//                break; 
+
+// } }
+//chap 21 to 38 task7
+
+// for (var i = 0; i < text.length; i++) {
+//        if (text.slice(i, i + 12) === "Hyderabad") { 
+//              text = text.slice(0, 1) + "Islam" + text.slice(i + 12);    } 
+//      }
+//chap 35 to 38 task7
+
+// var now = new Date(); 
+//  var theHr = now.getHours(); 
+//  var theMin = now.getMinutes(); 
+//   alert("time: " + theHr + ":" + theMin);
